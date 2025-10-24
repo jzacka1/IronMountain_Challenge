f5fe7123883acfb115988cbcfef6283346ea3708
James Zacka <jameszacka@gmail.com>
2025-10-24 16:43:47 -0400

    Uploaded instruction and sql files.

---
+++ CreateEmployeeTable.sql
@@ -0,0 +1,25 @@
+
+/* CHANGE THE NAME OF THE DATABASE */
+USE [IronMontainDb]
+GO
+
+/****** Object:  Table [dbo].[Employees]    Script Date: 10/15/2025 12:22:08 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Employees](
+	[EmployeeID] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [nvarchar](30) NULL,
+	[LastName] [nvarchar](30) NULL,
+	[DOB] [datetime] NOT NULL,
+ CONSTRAINT [PK_dbo.Employees] PRIMARY KEY CLUSTERED 
+(
+	[EmployeeID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
